90d367a115d916dca56362f137cacb52c6d21ae5
Suraj <surajgupta.college@gmail.com>
2023-09-02 23:45:06 +0530

    added type safe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,74 +25,81 @@ import SortableLinks from '@/components/SortableLinks';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { AddNewItem } from '@/components/AddNewItem';
 
+// Define the item interface
+interface Item {
+  name: string;
+  id: number;
+}
 
-export default function Home() {
+interface HomeProps {
+  // You can add any additional props if needed
+}
+
+const Home: React.FC<HomeProps> = () => {
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
       coordinateGetter: sortableKeyboardCoordinates,
-    }),
+    })
   );
 
-  const [items, setItems] = useState([
-    { name: "NextJS", id: 1693653637084 },
-    { name: "ReactJS", id: 1693653637086 },
-    { name: "Astro", id: 1693653637088 },
-    { name: "Vue", id: 1693653637090 },
+  const [items, setItems] = useState<Item[]>([
+    { name: 'NextJS', id: 1693653637084 },
+    { name: 'ReactJS', id: 1693653637086 },
+    { name: 'Astro', id: 1693653637088 },
+    { name: 'Vue', id: 1693653637090 },
   ]);
 
   function handleDragEnd(event: any) {
     const { active, over } = event;
 
     if (active.id !== over.id) {
-      setItems((items) => {
-        const oldIndex = items.findIndex(list => list.id === active.id)
-        const newIndex = items.findIndex(list => list.id === over.id)
+      setItems((prevItems) => {
+        const oldIndex = prevItems.findIndex((item) => item.id === active.id);
+        const newIndex = prevItems.findIndex((item) => item.id === over.id);
 
-        return arrayMove(items, oldIndex, newIndex);
+        return arrayMove(prevItems, oldIndex, newIndex);
       });
     }
   }
 
   function handleDelete(idToDelete: number) {
-    setItems((items) => items.filter((item) => item.id !== idToDelete));
+    setItems((prevItems) => prevItems.filter((item) => item.id !== idToDelete));
   }
 
-  let idx = Date.now()
-  function addNewItem(newItem: any) {
-    setItems((prevItems) => [
-      ...prevItems,
-      { name: newItem, id: idx },
-    ]);
+  let idx = Date.now();
+
+  function addNewItem(newItem: string) {
+    setItems((prevItems) => [...prevItems, { name: newItem, id: idx }]);
   }
+
   return (
     <main className='flex justify-center items-center h-screen px-2 mx-auto select-none'>
       <Card className='w-full md:max-w-lg'>
-        <CardHeader className="space-y-1 ">
-          <CardTitle className="text-2xl flex justify-between">
+        <CardHeader className='space-y-1 '>
+          <CardTitle className='text-2xl flex justify-between'>
             Frameworks
             <AddNewItem addNewItem={addNewItem} />
           </CardTitle>
-          <CardDescription>
-            List Popular web development frameworks
-          </CardDescription>
+          <CardDescription>List Popular web development frameworks</CardDescription>
         </CardHeader>
-        <CardContent className="grid gap-4">
+        <CardContent className='grid gap-4'>
           <DndContext
             sensors={sensors}
             collisionDetection={closestCenter}
             onDragEnd={handleDragEnd}
             modifiers={[restrictToVerticalAxis, restrictToParentElement]}
           >
-            <SortableContext
-              items={items}
-              strategy={verticalListSortingStrategy}
-            >
-              {items.map(id => <SortableLinks key={id.id} id={id} onDelete={handleDelete} />)}
+            <SortableContext items={items} strategy={verticalListSortingStrategy}>
+              {items.map((item) => (
+                <SortableLinks key={item.id} id={item} onDelete={handleDelete} />
+              ))}
             </SortableContext>
           </DndContext>
         </CardContent>
       </Card>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
--- src/components/SortableLinks.tsx
+++ src/components/SortableLinks.tsx
@@ -1,17 +1,20 @@
-"use client"
-
-import React, { FC } from 'react'
-import { Card } from '@/components/ui/card'
+import React, { FC } from 'react';
+import { Card } from '@/components/ui/card';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 
+interface Item {
+    id: number;
+    name: string;
+}
+
 interface SortableLinkCardProps {
-    id: any
-    onDelete: any
+    id: Item;
+    onDelete: (id: number) => void;
 }
 
 const SortableLinks: FC<SortableLinkCardProps> = ({ id, onDelete }) => {
-    const uniqueId = id.id
+    const uniqueId = id.id;
     const {
         attributes,
         listeners,
@@ -25,30 +28,30 @@ const SortableLinks: FC<SortableLinkCardProps> = ({ id, onDelete }) => {
         transition,
     };
 
-    const handleButtonClick = (props: any) => {
-        // Pass the id to the onDelete function
-        onDelete(props);
+    const handleButtonClick = () => {
+        onDelete(uniqueId);
     };
+
+    const isCursorGrabbing = attributes['aria-pressed'];
+
     return (
         <div ref={setNodeRef} style={style} key={uniqueId}>
-            <Card className='p-4 relative flex justify-between gap-5 group' >
-                <div>
-                    {id.name}
-                </div>
+            <Card className='p-4 relative flex justify-between gap-5 group'>
+                <div>{id.name}</div>
                 <div className='flex justify-center items-center gap-4'>
-                    <button className="hidden group-hover:block " onClick={() => handleButtonClick(uniqueId)}>
+                    <button className="hidden group-hover:block" onClick={handleButtonClick}>
                         <svg className='text-red-500' xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
                     </button>
-                    <button className="bg-white" {...attributes} {...listeners} aria-describedby={`DndContext-${uniqueId}`} >
+                    <button {...attributes} {...listeners} className={` ${isCursorGrabbing ? 'cursor-grabbing' : 'cursor-grab'}`} aria-describedby={`DndContext-${uniqueId}`}>
                         <svg viewBox="0 0 20 20" width="15">
                             <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"
-                                fill={"currentcolor"}></path>
+                                fill="currentColor"></path>
                         </svg>
                     </button>
                 </div>
             </Card>
         </div>
-    )
-}
+    );
+};
 
-export default SortableLinks
+export default SortableLinks;
